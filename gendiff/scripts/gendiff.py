bfbb04da0221964784dd998dba64242458982194
Olha Devlysh <odevlysh@gmail.com>
2024-10-05 14:24:31 -0400

    Step 1: Add positional arguments for first and second files to gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,6 +1,9 @@
 def main():
     import argparse
-    parser = argparse.ArgumentParser()
-    parser.parse_args()
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file', help='First file to compare')
+    parser.add_argument('second_file', help='Second file to compare')
+    args = parser.parse_args()
+
 if __name__ == '__main__':
     main()
