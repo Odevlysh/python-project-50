17b0cd9501034e239d2a9bc3a3351d2774789551
Olha Devlysh <odevlysh@gmail.com>
2024-10-06 17:37:34 -0400

    Add file reading and parsing functionality for JSON configuration files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 def main():
     import argparse
+    import json
     
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     
@@ -13,6 +14,16 @@ def main():
     )
     
     args = parser.parse_args()
+
+    with open(args.first_file, 'r') as file1:
+        file1_data = json.load(file1)
+
+    with open(args.second_file, 'r') as file2:
+        file2_data = json.load(file2)
+
+    # tmp - for testing
+    print('Data from the first file:', file1_data)
+    print('Data from the secosnd file:', file2_data)
 
 if __name__ == '__main__':
     main()
