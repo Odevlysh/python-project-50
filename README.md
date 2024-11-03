95e048849bd450203b58ff72003c0a0c772d37f5
Olha Devlysh <odevlysh@gmail.com>
2024-11-03 15:03:50 -0500

    Step 4: Implement flat JSON file comparison, add library support for generate_diff, and update README with asciinema demo

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Odevlysh/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Odevlysh/python-project-50/actions)
+
+### You can see a demo of the step 4 here:
+https://asciinema.org/a/2SmCRloV6LYx2hDX2flcyw5Eg
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,3 +1,39 @@
+def generate_diff(file1_data, file2_data):
+
+    differences = {}
+    all_keys = sorted(file1_data.keys() | file2_data.keys()) 
+
+    for key in all_keys:
+        if key in file1_data and key in file2_data:
+            if file1_data[key] != file2_data[key]:
+                differences[key] = ('changed', file1_data[key], file2_data[key])
+            elif file1_data[key] == file2_data[key]:
+                differences[key] = ('same', file1_data[key])
+        elif key in file1_data:
+            differences[key] = ('removed', file1_data[key])
+        else:
+            differences[key] = ('added', file2_data[key])
+
+    return format_diff(differences)
+
+
+def format_diff(differences):
+    result = []
+
+    for key, value in differences.items():
+        if value[0] == 'removed':
+            result.append(f"  - {key}: {value[1]}")
+        elif value[0] == 'added':
+            result.append(f"  + {key}: {value[1]}")
+        elif value[0] == 'changed':
+            result.append(f"  - {key}: {value[1]}")
+            result.append(f"  + {key}: {value[2]}")
+        elif value[0] == 'same':
+            result.append(f"    {key}: {value[1]}")
+
+    return "{\n" + "\n".join(result) + "\n}"
+            
+
 def main():
     import argparse
     import json
@@ -22,8 +58,12 @@ def main():
         file2_data = json.load(file2)
 
     # tmp - for testing
-    print('Data from the first file:', file1_data)
-    print('Data from the secosnd file:', file2_data)
+    # print('Data from the first file:', file1_data)
+    # print('Data from the secosnd file:', file2_data)
+    
+    result = generate_diff(file1_data, file2_data)
+    print(result)
+
 
 if __name__ == '__main__':
     main()
